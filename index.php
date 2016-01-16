f85cbbd19cf1d856a4b2cd157bc1b1a4898db843
Manojit Ballav <manojitocto@gmail.com>
2016-01-16 08:43:43 +0530

    Initial

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once('init.php');
+// START 1
+require_once('process.php');
+// END 1
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <title>The Rate</title>
+    </head>
+    <body>
+        <header>
+            <h1>Rate Us</h1>
+        </header>
+        <p id="first">Were we let in for our looks? Hell No. Will we be judged on them? Yes Bitch.</p>
+        <p id="second">Am I Hotter? Click to Choose.</p>
+        <div id="dual">
+            <?php require_once('ajax/ajax.dual.php'); ?>
+        </div>
+        <footer>Pictures from <a href="http://www.facebook.com" target="_blank">Facebook.com</a></footer>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $(".photos").click(function() {
+                    $("#loader").fadeIn("fast");
+                    var tokenFirst  = $(".faces:first-child .photos").attr("data-token"),
+                        tokenSecond = $(".faces:last-child .photos").attr("data-token"),
+                        scoreFirst  = $(".faces:first-child .photos").attr("data-score"),
+                        scoreSecond = $(".faces:last-child .photos").attr("data-score"),
+                        winner,
+                        looser,
+                        wScore,
+                        lScore;
+
+                        if (tokenFirst == $(this).attr("data-token"))
+                        {
+                            winner = tokenFirst;
+                            looser = tokenSecond;
+                            wScore = scoreFirst;
+                            lScore = scoreSecond;
+                        }
+                        else
+                        {
+                            winner = tokenSecond;
+                            looser = tokenFirst;
+                            wScore = scoreSecond;
+                            lScore = scoreFirst;
+                        }
+
+                    $.ajax({
+                        type: "post",
+                        url: "index.php",
+                        data: "winner=" + winner + "&looser=" + looser + "&wScore=" + wScore + "&lScore=" + lScore,
+                        cache: false,
+                        success: function(data) {
+                            $("body").html(data);
+                            $("#loader").fadeOut("fast");
+                        }
+                    });
+                });
+            });
+        </script>
+    </body>
+</html>
---
+++ init.php
@@ -0,0 +1,3 @@
+<?php
+$mysqli = new mysqli('db_url', 'db_user', 'db_password', 'db_name');
+?>
